e81b3ec92693df7d2a98feff3f5c1157af3f34b0
Daffa Abiyyu <daffaabiyyu04@gmail.com>
2024-06-11 13:00:17 +0700

    create project

---
+++ src/main/java/id/ac/astra/polytechnic/internakbe/InTernakBeApplication.java
@@ -0,0 +1,13 @@
+package id.ac.astra.polytechnic.internakbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InTernakBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InTernakBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/astra/polytechnic/internakbe/InTernakBeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.astra.polytechnic.internakbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InTernakBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
